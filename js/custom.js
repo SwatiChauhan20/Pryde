d7d8cde067dbcc24823ee341a028491a658dae59
SwatiChauhan20 <[chauhan.swati104@gmail.com]>
2019-06-21 15:14:36 +0530

    first pull

---
+++ js/custom.js
@@ -0,0 +1,75 @@
+// doctor cunsultation page slidetoggle button js
+$(document).ready(function(){
+  $(".specialities_toggle_btn").click(function(){
+    $(".specialities_toggle_menu").slideToggle();
+  });
+});
+// My Appointment calendar js
+ $(document).ready(function() {
+ 	$('#calendar').fullCalendar({
+ 		editable: true,
+ 		eventLimit: true,
+ 		updateEvent: true, 
+ 		addEvent:true,
+ 		events: [
+ 		{
+ 			title  : '3:00PM - 4:00PM',
+ 			start  : '2019-06-01'
+ 		},
+ 		{
+ 			title  : '3:00PM - 4:00PM',
+ 			start  : '2010-01-05',
+
+ 		},
+ 		{
+ 			title  : '3:00PM - 4:00PM',
+ 			start  : '2019-06-15',
+ 		}
+ 		]
+ 	});
+ });
+
+// search clinic page js start
+
+
+// second opinion page js
+
+$(document).ready(function(){
+	$('.doctor_slider').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: '<button type="button" class="btn slick-prev" data-role="none"><i class="fa fa-caret-left" aria-hidden="true"></i></button>',
+		nextArrow: '<button type="button" class="btn slick-next" data-role="none"><i class="fa fa-caret-right" aria-hidden="true"></i></button',
+		responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
+		{	
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 577,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+});
