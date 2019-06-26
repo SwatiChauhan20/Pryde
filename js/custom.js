eb4c522c9e59b625b08a55d558bb49db815699fa
SwatiChauhan20 <[chauhan.swati104@gmail.com]>
2019-06-26 15:31:53 +0530

    linking

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,118 @@
+$(window).on('load',function() {
+	setTimeout(function(){
+		$('#loader-wrapper').fadeOut('slow', function(){
+			$('body').removeAttr('style');
+		});
+	}, 1500);
+});
+$(document).ready(function(){
+	$('.side_search_content a').click(function(){
+		$('.right_side_search').toggleClass('open_search');
+	});
+	$('.side_search_content').click(function (et) {
+        et.stopPropagation();
+    });
+	$(document).click(function (ent) {
+        ent.stopPropagation();
+        $(".right_side_search").removeClass('open_search');
+    });
+	$('.back_to_top').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+	});
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > 600) {
+			$('.back_to_top').fadeIn();
+		} else {
+			$('.back_to_top').fadeOut();
+		}
+	});
+
+	$('.doctor_slider').slick({
+		dots: false,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		nextArrow: '<button class="slick-next slick-arrow" type="button"><i class="fa fa-caret-right" aria-hidden="true"></i></button>',
+		prevArrow: '<button class="slick-prev slick-arrow" type="button"><i class="fa fa-caret-left" aria-hidden="true"></i></button>',
+		draggable: false,
+		responsive: [
+		{
+			breakpoint: 1367,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3
+			}
+		},
+		{
+			breakpoint: 870,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 620,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+
+	$('.customer_slider').slick({
+		dots: false,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		arrows: false,
+		autoplay: true,
+		draggable: true,
+		responsive: [
+		{
+			breakpoint: 1367,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3
+			}
+		},
+		{
+			breakpoint: 870,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+});
+
+
+
+
+
 // doctor cunsultation page slidetoggle button js
 $(document).ready(function(){
   $(".specialities_toggle_btn").click(function(){
@@ -33,47 +148,6 @@ $(document).ready(function(){
  	});
  });
 
-// second opinion page js
-
-$(document).ready(function(){
-	$('.doctor_slider').slick({
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		arrows: true,
-		prevArrow: '<button type="button" class="btn slick-prev" data-role="none"><i class="fa fa-caret-left" aria-hidden="true"></i></button>',
-		nextArrow: '<button type="button" class="btn slick-next" data-role="none"><i class="fa fa-caret-right" aria-hidden="true"></i></button',
-		responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 4,
-				slidesToScroll: 1,
-			}
-		},
-		{
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
-			}
-		},
-		{	
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 577,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-		]
-	});
-});
 
 // appoinment booking based on doctor
 
@@ -123,3 +197,48 @@ $(document).ready(function(){
 		]
 	});
 });
+
+$(document).ready(function() {
+ 	$('#calender_dashboard').fullCalendar({
+ 		editable: true,
+ 		eventLimit: true,
+ 		updateEvent: true, 
+ 		addEvent:true,
+ 		events: [
+ 		{
+ 			title  : 'doctor',
+ 			time :'3:00PM - 4:00PM',
+ 			start  : '2019-06-01'
+ 		},
+ 		{
+ 			title  : '3:00PM - 4:00PM',
+ 			start  : '2010-01-05',
+
+ 		},
+ 		{
+ 			title  : '3:00PM - 4:00PM',
+ 			start  : '2019-06-15',
+ 		}
+ 		]
+ 	});
+ });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+// css start landing page
+
